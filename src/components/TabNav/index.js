d30b6d1724e800846a9a37a00445c756b765ecb8
RajaPradhan <rajapradhan08@gmail.com>
2017-09-24 08:12:16 +0530

    Add tab navigation in small screen

--- src/components/TabNav/index.js
+++ src/components/TabNav/index.js
@@ -1,28 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import classNames from "classnames";
+
+import * as actions from "actions";
 
 import baseStyles from "containers/App/styles.scss";
 import styles from "./styles.scss";
 
-const TabNav = () => {
-  return (
-    <div
-      className={classNames(
-        "row",
-        baseStyles["hide-on-med-and-above"],
-        styles["tabs-container"]
-      )}
-      style={{ color: "#fff" }}
-    >
-      <div className="col s12">
-        <ul className={styles.tabs}>
-          <li className={styles.tab}>Currently Running</li>
-          <li className={styles.tab}>Most Polular</li>
-          <li className={styles.tab}>Recent Movies</li>
-        </ul>
+class TabNav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleTabChange = this.handleTabChange.bind(this);
+  }
+
+  handleTabChange(event) {
+    const selectedMovieCategory = event.target.textContent;
+
+    switch (selectedMovieCategory) {
+      case "Now playing":
+        this.props.setMoviesCategory("nowPlayingMovies");
+        this.props.fetchNowPlayingMovies();
+        break;
+      case "Top rated":
+        this.props.setMoviesCategory("topRatedMovies");
+        this.props.fetchTopRatedMovies();
+        break;
+      case "Upcoming":
+        this.props.setMoviesCategory("upcomingMovies");
+        this.props.fetchUpcomingMovies();
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={classNames(
+          "row",
+          baseStyles["hide-on-med-and-above"],
+          styles["tabs-container"]
+        )}
+        style={{ color: "#fff" }}
+      >
+        <div className="col s12">
+          <ul className={styles.tabs}>
+            <li
+              className={styles.tab}
+              value="nowPlayingMovies"
+              onClick={this.handleTabChange}
+            >
+              Now playing
+            </li>
+            <li
+              className={styles.tab}
+              value="topRatedMovies"
+              onClick={this.handleTabChange}
+            >
+              Top rated
+            </li>
+            <li
+              className={styles.tab}
+              value="upcomingMovies"
+              onClick={this.handleTabChange}
+            >
+              Upcoming
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = ({ movies }) => {
+  return { movies };
 };
 
-export default TabNav;
+const mapDispatchToProps = {
+  setMoviesCategory: actions.setMoviesCategory,
+  fetchNowPlayingMovies: actions.fetchNowPlayingMovies,
+  fetchTopRatedMovies: actions.fetchTopRatedMovies,
+  fetchUpcomingMovies: actions.fetchUpcomingMovies
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TabNav);
