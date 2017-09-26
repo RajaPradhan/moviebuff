10f46eef7c028e968990b04977b04669903fcde1
RajaPradhan <rajapradhan08@gmail.com>
2017-09-26 07:30:35 +0530

    Redirect http to https in express

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const path = require("path");
 
+const NODE_ENV = process.env.NODE_ENV || "development";
 const PORT = process.env.PORT || 9000;
+
 const app = express();
 
-const INDEX_FILE = path.join(__dirname, '/build/index.html');
+const INDEX_FILE = path.join(__dirname, "/build/index.html");
 
-app.use(express.static(__dirname + '/build'));
+// Redirect http requests to use https in production
+if (NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+      next();
+    }
+  });
+}
+
+app.use(express.static(__dirname + "/build"));
 
 app.get("*", (req, res) => res.sendFile(INDEX_FILE));
 
